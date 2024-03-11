808227b9de4800e2738ff36f57201b0f1d239593
domgud <dgdomis52@gmail.com>
2024-03-11 15:27:41 +0200

    Input validation

--- app/src/main/java/com/example/fintech_l2/MainActivity.java
+++ app/src/main/java/com/example/fintech_l2/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.fintech_l2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -23,17 +25,45 @@ public class MainActivity extends AppCompatActivity {
 
         initializeComponents();
 
+        // set button action
         calculateButton.setOnClickListener(v -> buttonOnClickCalculate());
     }
 
-    private void buttonOnClickCalculate(){
-        Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show();
+    private void initializeComponents(){
+        calculateButton = (Button) findViewById(R.id.btnCalculate);
+        inputText = (EditText) findViewById(R.id.txtInput);
+        resultText = (TextView) findViewById(R.id.tvResult);
+        choicesSpinner = (Spinner) findViewById(R.id.choicesSpinner);
+
+        // init values for spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.calculation_choices_array,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        choicesSpinner.setAdapter(adapter);
     }
 
-    private void initializeComponents(){
-        calculateButton = findViewById(R.id.btnCalculate);
-        inputText = findViewById(R.id.txtInput);
-        resultText = findViewById(R.id.tvResult);
-        choicesSpinner = findViewById(R.id.choicesSpinner);
+    private void buttonOnClickCalculate(){
+        boolean validationPassed = validateButtonInput();
+        if (!validationPassed){
+            return;
+        }
+        
     }
+
+    /**
+     * @return true if validation passed, false otherwise
+     */
+    private boolean validateButtonInput(){
+        String strInput = inputText.getText().toString();
+        if(TextUtils.isEmpty(strInput)) {
+            inputText.setError(getString(R.string.empty_input));
+            Toast.makeText(this, getString(R.string.empty_input), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
 }
